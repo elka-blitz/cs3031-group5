ea02aa6a5ac306f155d2fb71f3e2bd5c97fdf105
elka-blitz <122494314@umail.ucc.ie>
2025-11-04 21:19:57 +0000

    Clear commented code

--- nav_demo.py
+++ nav_demo.py
@@ -14,15 +14,6 @@ Check for the pin raw values
 Check raw value every main process cycle and act from there
 Example commented below
 '''
-# while True:
-#     raw_value = touch_A1.raw_value
-#     if raw_value > 3000:
-#         print('touch detected ' + str(raw_value))
-#         cpx.pixels.fill([255, 255, 255])
-#     else:
-#         cpx.pixels.fill([0,0,0])
-#     time.sleep(0.1)
-
 nav = group5StudyAssistantNavigation()
 
 while True:
