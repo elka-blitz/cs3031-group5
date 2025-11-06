ed8d030dbcb43c6eb9379fac605b181753df7592
elka-blitz <122494314@umail.ucc.ie>
2025-11-06 18:32:08 +0000

    Correct navbuttons

--- navbuttons.py
+++ navbuttons.py
@@ -1,17 +1,24 @@
 from adafruit_circuitplayground.express import cpx
-from touchio import TouchIn
-from board import A1
+import time
+import touchio
+import board
 
 class group5StudyAssistantNavigation():
     def __init__(self):
         self.var1 = 0
         cpx.pixels.brightness = 0.1
-        self.touch_A1 = TouchIn(A1)
+        self.touch_A1 = touchio.TouchIn(board.A1)
+        self.touch_A2 = touchio.TouchIn(board.A2)
 
 
-    def touch(self):
+    def touch_a1(self):
         if self.touch_A1.raw_value > 3000:
             return True
         else:
             return False
-    
+        
+    def touch_a2(self):
+        if self.touch_A2.raw_value > 3000:
+            return True
+        else:
+            return False
