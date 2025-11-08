61091f64068f2070d9e9cb700df5e16e9c9ac246
elka-blitz <122494314@umail.ucc.ie>
2025-11-08 21:43:38 +0000

    Refactor main process

--- code.py
+++ code.py
@@ -1,91 +1,65 @@
 from lcd16x2 import LCD_16x2
-from time import sleep, time
-from adafruit_circuitplayground.express import cpx
-from navbuttons import group5StudyAssistantNavigation
+from time import sleep
 from led_controller import led_controller
-from nfc_reader import nfc_reader
+from navbuttons import group5StudyAssistantNavigation
+from time import sleep, time
+from adafruit_circuitplayground import cp
 
+prox = led_controller()
+nav = group5StudyAssistantNavigation()
+lcd = LCD_16x2()
+prox.update_lights(False)
+# nfc = nfc_reader('0x80x', 5)
 
-cpx.pixels.brightness = 0.1
-STUDY_ASSISTANT_STATE = 0
+SHELF_STATE, STATE_IDLE, STATE_PHONE_NOT_DETECTED, STATE_SET_TIMER, STATE_COUNTDOWN = [i for i in range(0, 5)]
 
 DO_ANIMATION = True
 ANIMATION_CYCLE = False
 ANIMATION_FRAME_TICK = 0
 ANIMATION_UPDATE = True
-FRAME_LENGTH = 1
+FRAME_LENGTH = 2
 
-TIME_SET = False
-TIME_NAV = 20
-TIME_AMOUNT_SET = 0
-START_TIME = 0
-TIME_SESSION_COMPLETE = False
+TIME_REMAINING = 100
+TIME_INDEX = ''
 
-lcd = LCD_16x2()
-nav = group5StudyAssistantNavigation()
-nfc = nfc_reader('0x80x', 5)
-led = led_controller()
+def lcd_page(page_no):
+    global TIME_REMAINING
+    global ANIMATION_FRAME_TICK
+    global ANIMATION_CYCLE
+    ANIMATION_FRAME_TICK += 1
+    if ANIMATION_FRAME_TICK > FRAME_LENGTH:
+        ANIMATION_CYCLE = not ANIMATION_CYCLE
+        ANIMATION_FRAME_TICK = 0
+
+    line_1a,line_2a,line_1b,line_2b = ('Place phone','in shelf','To start','Study session') if page_no == STATE_IDLE else ('Studying...', str(TIME_REMAINING), 'Studying...', str(TIME_REMAINING)) if page_no == STATE_COUNTDOWN else ('Phone not', 'detected', 'Place phone', 'in shelf') if page_no == STATE_PHONE_NOT_DETECTED else ('Set timer', TIME_INDEX, 'Set timer', TIME_INDEX) if page_no == STATE_SET_TIMER else ('error', 'e', 'e', 'error')
+
+    if ANIMATION_CYCLE:
+        lcd.display_message(line_1a, line_2=line_2a)
+    else:
+        lcd.display_message(line_1b, line_2=line_2b)
 
 while True:
-    sleep(0.5)
+    sleep(1)
     if DO_ANIMATION: 
         ANIMATION_FRAME_TICK += 1
     if ANIMATION_FRAME_TICK > FRAME_LENGTH:
         ANIMATION_CYCLE = not ANIMATION_CYCLE 
         ANIMATION_FRAME_TICK = 0
-    drawer_closed = led.is_closed() 
-    phone_placed = nfc.getPhoneState() 
+    drawer_closed = prox.is_closed() 
+    # phone_placed = nfc.getPhoneState()
+    phone_placed = cp.switch
 
-    if drawer_closed and phone_placed and not TIME_SESSION_COMPLETE:
-        if TIME_SET != True: 
-            TIME_AMOUNT_SET = TIME_NAV 
-            TIME_SET = True
-            START_TIME = time()
-        elapsed_time = time() - START_TIME
-        remaining_time = round(((TIME_AMOUNT_SET - elapsed_time) / 60) * 100)
-        remaining_time_percentage = round(remaining_time / TIME_AMOUNT_SET) 
-        if remaining_time < 0:
-            TIME_SESSION_COMPLETE = True
-        DO_ANIMATION = False 
-        lcd.display_message('Studying...', line_2=str(remaining_time))
+    SHELF_STATE = STATE_IDLE if not drawer_closed and not phone_placed else STATE_PHONE_NOT_DETECTED if drawer_closed and not phone_placed else STATE_SET_TIMER if phone_placed and not drawer_closed else STATE_COUNTDOWN if phone_placed and drawer_closed else 'error'
+    
+    lcd_page(SHELF_STATE)
 
-    elif not drawer_closed and not phone_placed:     
-        DO_ANIMATION = True
-        if ANIMATION_CYCLE != ANIMATION_UPDATE:
-            ANIMATION_UPDATE = ANIMATION_CYCLE
-            if ANIMATION_CYCLE:
-                lcd.display_message('Place phone', line_2='in shelf')
-            else:
-                lcd.display_message('To start', line_2='study session')
+    if SHELF_STATE == STATE_COUNTDOWN:
+        TIME_REMAINING -= 1
 
-    elif drawer_closed:      
-        if TIME_SESSION_COMPLETE:
-            DO_ANIMATION = True
-            if ANIMATION_CYCLE != ANIMATION_UPDATE:
-                ANIMATION_UPDATE = ANIMATION_CYCLE
-                if ANIMATION_CYCLE:
-                    lcd.display_message('Session complete')
-                else:
-                    lcd.display_message('Open shelf', line_2='to restart')
-                break
-        DO_ANIMATION = True
-        if ANIMATION_CYCLE != ANIMATION_UPDATE:
-            ANIMATION_UPDATE = ANIMATION_CYCLE
-
-            if ANIMATION_CYCLE:
-                lcd.display_message('Phone not', line_2='in shelf')
-            else:
-                lcd.display_message('Place phone in', line_2='shelf to start')
-
-    else:       
-        DO_ANIMATION = False
-        if nav.touch_a1():    
-            TIME_NAV += 5
-            lcd.display_message('Set time:', line_2=str(TIME_NAV))
+    elif SHELF_STATE == STATE_SET_TIMER:
+        if nav.touch_a1():
+            TIME_INDEX += 1
         if nav.touch_a2():
-            if TIME_NAV - 5 > 1: 
-                TIME_NAV = TIME_NAV - 5
-            lcd.display_message('Set time:', line_2=str(TIME_NAV))
-        if ANIMATION_CYCLE != ANIMATION_UPDATE:
-            ANIMATION_UPDATE = ANIMATION_CYCLE
-            lcd.display_message('Set time:', line_2=str(TIME_NAV))
+            TIME_INDEX -= 1
+    else:
+        print('error')
