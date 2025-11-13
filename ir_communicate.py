6f0fe99c7f5fdaff5d66807a5a5e5151fe6b4de8
elka-blitz <122494314@umail.ucc.ie>
2025-11-13 13:31:04 +0000

    Remove comments

--- ir_communicate.py
+++ ir_communicate.py
@@ -2,29 +2,6 @@ import adafruit_irremote
 import pulseio
 import board
 
-# # Create a 'pulseio' output, to send infrared signals on the IR transmitter @ 38KHz
-# pulseout = pulseio.PulseOut(board.IR_TX, frequency=38000, duty_cycle=2 ** 15)
-# # Create an encoder that will take numbers and turn them into NEC IR pulses
-# encoder = adafruit_irremote.GenericTransmit(header=[9000, 4500],
-#                                             one=[560, 1700],
-#                                             zero=[560, 560],
-#                                             trail=0)
-
-# while True:
-#     if cpx.button_a:
-#         print("Button A pressed! \n")
-#         cpx.red_led = True
-#         encoder.transmit(pulseout, [255, 2, 255, 0])
-#         cpx.red_led = False
-#         # wait so the receiver can get the full message
-#         time.sleep(0.2)
-#     if cpx.button_b:
-#         print("Button B pressed! \n")
-#         cpx.red_led = True
-#         encoder.transmit(pulseout, [255, 2, 191, 64])
-#         cpx.red_led = False
-#         time.sleep(0.2)
-
 class ir_shelfstate():
     def __init__(self):
         self.pulseout = pulseio.PulseOut(board.IR_TX, frequency=38000, duty_cycle=2 ** 15)
